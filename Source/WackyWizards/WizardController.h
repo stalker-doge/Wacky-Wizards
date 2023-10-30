46fc64cec68561c368c4267b5d7b2f9c8be5ad17
LucianMATAN <94372701+stalker-doge@users.noreply.github.com>
2023-10-30 18:07:08 +0000

    Introduced dynamic player starts

--- Source/WackyWizards/WizardController.h
+++ Source/WackyWizards/WizardController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "Wizard.h"
 #include "WizardController.generated.h"
 
 /**
@@ -14,13 +15,18 @@ class WACKYWIZARDS_API AWizardController : public APlayerController
 {
 	GENERATED_BODY()
 
+
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+
+
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
 
 	
 };
